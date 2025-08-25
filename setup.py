d9c2b9599094a5e15f26fdf9d76c71a901194f38
kunkunlin1221 <jacklin@cmlab.csie.ntu.edu.tw>
2025-08-25 22:21:41 +0800

    [C] Update docker and fix test error

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
+import subprocess
+import sys
 import sysconfig
 
-import numpy as np
-from Cython.Build import cythonize
 from setuptools import Extension, setup
 
-ext_modules = cythonize([
-    Extension(
-        "sim3dr_cython",
-        sources=[
-            "pyface/components/face_depth/Sim3DR/rasterize.pyx",
-            "pyface/components/face_depth/Sim3DR/rasterize_kernel.cpp",
-        ],
-        language="c++",
-        include_dirs=[np.get_include(), sysconfig.get_paths()["platinclude"]],
-        extra_compile_args=["-std=c++11"],
-    )
-])
-setup(ext_modules=ext_modules)
+if __name__ == "__main__":
+    subprocess.call([sys.executable, "-m", "pip", "install", "cython", "numpy"])
 
+    import numpy as np
+    from Cython.Build import cythonize
 
-# if __name__ == "__main__":
-#     prepare_requirements()
-#     import numpy as np
-
-#     setup(ext_modules=get_sim3dr_ext())
+    ext_modules = cythonize([
+        Extension(
+            "sim3dr_cython",
+            sources=[
+                "pyface/components/face_depth/Sim3DR/rasterize.pyx",
+                "pyface/components/face_depth/Sim3DR/rasterize_kernel.cpp",
+            ],
+            language="c++",
+            include_dirs=[np.get_include(), sysconfig.get_paths()["platinclude"]],
+            extra_compile_args=["-std=c++11"],
+        )
+    ])
+    setup(ext_modules=ext_modules)
